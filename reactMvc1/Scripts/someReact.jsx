aac4a3719038baefe64ad78b1ad223b4fa3d6e9c
Danieldanieljohnkerr@hotmail.com <danieljohnkerr@hotmail.com>
2017-09-27 21:34:51 +0100

    Moved to Visual Studio 2017...not sure ifi ti made any improvement. Installed ReactJs.Net packages - not sure if this made much difference. Moved ReactJs code to new someReact.jsx file - better IDE support in tbis than on cshtml page. .jsx seems treats element styling (eg color) in unexpected way. One of the changes made the relative paths go funny - I had to remove the "home/" part of the reference to the ajax target.

---
+++ reactMvc1/Scripts/someReact.jsx
@@ -0,0 +1,40 @@
+﻿//@* this code was in index.cshtml.seems you get better colours in jsx file...does it work? *@
+
+var HelloWorldComponent2 = React.createClass({
+    getInitialState: function () {
+        return {
+            serverMessage: ""
+        };
+    },
+    componentDidMount: function () {
+        //fetch data from server
+        $.get('getmessage', function (result) {
+            if (this.isMounted) {
+                this.setState({
+
+                    serverMessage: result
+                });
+            }
+        }.bind(this));
+    },
+    render: function () {
+        return (<h1 style={{color:"red" }}>{this.state.serverMessage}</h1>);
+    }
+
+});
+
+var another2 = React.createClass({
+    getInitialState: function () {
+        return {
+            serverMessage: ""
+        };
+    },
+    componentDidMount: function () {
+    },
+    render: function () {
+        return (<h1  >holt toledo</h1>);
+    }
+});
+
+//ReactDOM.render(<HelloWorldComponent />,document.getElementById("helloworldcontainer"));
+//ReactDOM.render(<another />, document.getElementById("cripe"));
